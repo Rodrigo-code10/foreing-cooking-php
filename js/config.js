58cc2a71207a3a6c5d3caac4d217c09b475ab8d8
Rodrigo-code10 <rodrigo.rolandogs@gmail.com>
2025-11-30 03:08:22 -0600

    Comunicación

--- js/config.js
+++ js/config.js
@@ -1,4 +1,2 @@
-const API_URL = window.location.hostname === "localhost"
-  ? "http://localhost:3000"
-  : "foreing-cooking-api-production.up.railway.app";
+const API_URL = "foreing-cooking-api-production.up.railway.app";
 export default API_URL;
