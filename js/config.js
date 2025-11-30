0b02a1012e37bd1561176fe31caf3ea772ed627b
Rodrigo-code10 <rodrigo.rolandogs@gmail.com>
2025-11-30 03:01:55 -0600

    Comunicación

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const API_URL = window.location.hostname === "localhost"
   ? "http://localhost:3000"
-  : "https://mi-backend.onrender.com";
+  : "foreing-cooking-api-production.up.railway.app";
 export default API_URL;
